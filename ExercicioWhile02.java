57f0a40d9eef4cb5cc53de9c1664dac12ff4a18d
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-04-24 11:17:50 -0300

    CEDUP - 24/04

--- ExercicioWhile02.java
+++ ExercicioWhile02.java
@@ -1,3 +1,15 @@
+import javax.swing.JOptionPane;
+
 public class ExercicioWhile02{
-    
+
+     public static void main(String[] args) {
+         
+        int idade = Integer.parseInt(JOptionPane.showInputDialog(
+            "Digite a sua idade\nPara sair digite um número maior que \"128\""));
+        
+        while (idade<=128) {
+            idade = Integer.parseInt(JOptionPane.showInputDialog(
+            "Digite a sua idade.\nPara sair digite um número maior que \"128\""));
+        }
+     }
 }
