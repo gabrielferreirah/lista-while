50c82919dcdb6f3d491a44eefb10d6400e5b4850
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-04-24 11:29:03 -0300

    CEDUP - 24/04

---
+++ ExercicioWhile03.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class ExercicioWhile03{
+
+    public static void main(String[] args) {
+        
+        double peso = Double.parseDouble(JOptionPane.showInputDialog("Digite seu Peso"));
+
+        int totalDePessoas = 0;
+        while ((peso > 0) || (peso)) {
+            
+        }
+    }
+}
