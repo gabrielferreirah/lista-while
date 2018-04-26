90ec8f32469a91e712cf89448951753857929952
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-04-26 08:41:57 -0300

    26/04 - CEDUP

--- ExercicioWhile02.java
+++ ExercicioWhile02.java
@@ -8,10 +8,16 @@ public class ExercicioWhile02{
             "Digite a sua idade\nPara sair digite um número maior que \"128\"",
             "Exercício Estrutura While 02", JOptionPane.PLAIN_MESSAGE));
         
+        int totalPessoas = 0;
+        
         while (idade<=128) {
             idade = Integer.parseInt(JOptionPane.showInputDialog(null,
             "Digite a sua idade\nPara sair digite um número maior que \"128\"",
             "Exercício Estrutura While 02", JOptionPane.PLAIN_MESSAGE));
+
+            totalPessoas = totalPessoas + 1;
+
+            System.out.println("total de pessoas: " + totalPessoas);
         }
      }
 }
--- ExercicioWhile03.java
+++ ExercicioWhile03.java
@@ -4,11 +4,17 @@ public class ExercicioWhile03{
 
     public static void main(String[] args) {
         
-        double peso = Double.parseDouble(JOptionPane.showInputDialog("Digite seu Peso"));
+        double peso = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite seu Peso", "Exercício 03", 
+        JOptionPane.PLAIN_MESSAGE));
 
         int totalDePessoas = 0;
-        while ((peso > 0) || (peso)) {
-            
+        while (peso > 0  && peso < 301) {
+            peso = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite seu Peso", "Exercício",
+            JOptionPane.PLAIN_MESSAGE));
+
+            totalDePessoas = totalDePessoas + 1;
         }
+
+        JOptionPane.showMessageDialog(null, "Quantidade de pessoas que informaram seu peso: " + totalDePessoas);
     }
 }
