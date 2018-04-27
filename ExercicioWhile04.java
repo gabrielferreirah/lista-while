59bb2295ae9d7d3c109c12d5b448532aa1a6ef83
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-04-27 08:35:28 -0300

    CEDUP - 27/04
    FINALIZADO

--- ExercicioWhile04.java
+++ ExercicioWhile04.java
@@ -4,6 +4,8 @@ import javax.swing.JOptionPane;
 
 public class ExercicioWhile04{
 
+    private static DecimalFormat df2 = new DecimalFormat(".##");
+
     public static void main(String[] args) {
 
         int quantidadeCarros = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantidade de carros",
@@ -14,17 +16,26 @@ public class ExercicioWhile04{
         int anoCarro = 0;
         int totalCarros = 0;
         int mediaAno = 0;
+        int quantidadeA = 0, quantidadeG = 0;
         double mediaPreco = 0;
 
         while (totalCarros != quantidadeCarros) {
             modelo = JOptionPane.showInputDialog(null, "Modelo do carro",
-                "Exercício While 04", JOptionPane.PLAIN_MESSAGE).trim();
+                "Exercício While 04", JOptionPane.PLAIN_MESSAGE).trim().toLowerCase();
 
             preco =Double.parseDouble(JOptionPane.showInputDialog(null, "Valor do carro",
                 "Exercício While 04", JOptionPane.PLAIN_MESSAGE));
 
             int anoCarros = Integer.parseInt(JOptionPane.showInputDialog(null, "Ano do carro",
                 "Exercício While 04", JOptionPane.PLAIN_MESSAGE));
+
+            char letra = modelo.charAt(0);
+
+            if (letra == 'a') {
+                quantidadeA = quantidadeA + 1;
+            } else if (letra == 'g') {
+                quantidadeG = quantidadeG + 1;
+            }
 
             totalCarros = totalCarros + 1; 
             anoCarro = anoCarro +anoCarros;
@@ -33,7 +44,9 @@ public class ExercicioWhile04{
 
         mediaAno = anoCarro/quantidadeCarros;
         JOptionPane.showMessageDialog(null, 
-        "\nMedia de valor dos carros: R$ " + mediaPreco + 
-        "\nMedia de anos dos carros: " + mediaAno);
+        "\nMedia de valor dos carros: R$ " + df2.format(mediaPreco) + 
+        "\nMedia de anos dos carros: " + mediaAno +
+        "\nQuantidade de carros que começam com a letra A: " + quantidadeA +
+        "\nQuantidade de carros que começam com a letra G: " + quantidadeG);
     }
 }
